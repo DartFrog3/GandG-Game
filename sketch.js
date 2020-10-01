5b107c40d04c3d1d8c040d2118b9de8029900245
DartFrog3 <colerobins03@gmail.com>
2020-10-01 09:53:35 -0700

    Pseudo and Asset

---
+++ sketch.js
@@ -0,0 +1,37 @@
+let location;
+
+function preload() {
+
+}
+
+function setup() {
+
+}
+
+function draw() {
+
+}
+class rooms {
+
+    constructor(roomsList, car, y, z) { 
+        this.roomsList = roomsList;
+        this.car = car;
+        this.y = y;
+        this.z = z;
+    }
+
+    roomsList = ["car"];
+
+    createCar() {
+        //put in background image
+    }
+
+
+    
+
+}
+
+function changeRooms () {
+    //location = new room
+    //change background and sprites
+}
