440d68b72ff52824ec25eca0bd2ec8a8e0bfe92c
DartFrog3 <colerobins03@gmail.com>
2020-10-02 09:57:36 -0700

    Game mobility

--- sketch.js
+++ sketch.js
@@ -1,15 +1,49 @@
-let location;
+//let location;
+
+const { thistle } = require("color-name");
+
+let arrowLeft;
+let arrowLeftImg;
+let arrowRight;
+let arrowRightImg;
+let arrowUp;
+let arrowUpImg;
+let arrowDown;
+let arrowDownImg;
+let dirKey;
 
 function preload() {
+    arrowLeftImg = loadImage('sprites/arrowLeft.png');
 
 }
 
 function setup() {
+    createCanvas(800, 800);
+    background('red');
 
 }
 
 function draw() {
+    arrowLeft = createSprite(75, 325);
+    arrowLeftImg.resize(50, 50);
+    arrowLeft.addImage(arrowLeftImg);
+    drawSprite(arrowLeft);
 
+}
+
+class arrow {
+    constructor (left, right, up, down, dirKey) {
+        this.left = arrowLeft;
+        this.right = arrowRight;
+        this.up = arrowUp;
+        this.down = arrowDown;
+        this.dirKey = dirKey;
+
+    }
+
+    drawArrow(dir) {
+        //make it create arrow off dir list
+    }
 }
 class rooms {
 
