f652807ce64da222feaf1c4b92530160fe2c3923
DartFrog3 <colerobins03@gmail.com>
2020-10-05 09:53:59 -0700

    Work on arrows and rooms

--- sketch.js
+++ sketch.js
@@ -1,7 +1,3 @@
-//let location;
-
-const { thistle } = require("color-name");
-
 let arrowLeft;
 let arrowLeftImg;
 let arrowRight;
@@ -11,61 +7,94 @@ let arrowUpImg;
 let arrowDown;
 let arrowDownImg;
 let dirKey;
+let carImg;
+let rooms;
+
 
 function preload() {
     arrowLeftImg = loadImage('sprites/arrowLeft.png');
+    arrowRightImg = loadImage('sprites/arrowRight.png');
+    arrowUpImg = loadImage('sprites/arrowUp.png');
+    arrowDownImg = loadImage('sprites/arrowDown.png');
+    carImg = loadImage('assets/car.jpg');
+
 
 }
 
 function setup() {
     createCanvas(800, 800);
-    background('red');
 
 }
 
 function draw() {
-    arrowLeft = createSprite(75, 325);
-    arrowLeftImg.resize(50, 50);
-    arrowLeft.addImage(arrowLeftImg);
-    drawSprite(arrowLeft);
-
+    rooms.car.display();
+    createArrows();
 }
 
-class arrow {
-    constructor (left, right, up, down, dirKey) {
-        this.left = arrowLeft;
-        this.right = arrowRight;
-        this.up = arrowUp;
-        this.down = arrowDown;
-        this.dirKey = dirKey;
+arrow = {
+    left : {
+        display : function() {
+            arrowLeft = createSprite(75, 325);
+            arrowLeftImg.resize(50, 50);
+            arrowLeft.addImage(arrowLeftImg);
+        }
+    },
 
-    }
+    right : {
+        display : function() {
+            arrowRight = createSprite(725, 325);
+            arrowRightImg.resize(50, 50);
+            arrowRight.addImage(arrowRightImg);
+        }
+    },
 
-    drawArrow(dir) {
-        //make it create arrow off dir list
+    up : {
+        display : function() {
+            arrowUp = createSprite(400, 65);
+            arrowUpImg.resize(50, 50);
+            arrowUp.addImage(arrowUpImg);
+        }
+    },
+
+    down : {
+        display : function() {
+            arrowDown = createSprite(400, 700);
+            arrowDownImg.resize(50, 50);
+            arrowDown.addImage(arrowDownImg);
+        }
     }
 }
-class rooms {
 
-    constructor(roomsList, car, y, z) { 
-        this.roomsList = roomsList;
-        this.car = car;
-        this.y = y;
-        this.z = z;
+function createArrows() { //adjust function to show only revlevant arrows
+    arrow.left.display();
+    arrow.right.display();
+    arrow.up.display();
+    arrow.down.display();
+    drawSprites();
+} // also must make arrows clickable and have the changeRooms function onClick
+
+rooms = {
+    car : {
+        index : 0,
+        display : function() {
+            background(carImg);
+        }
+    },
+
+    // end room, several rooms (10? maybe 12) with puzzles accross them- usually one main puzzle room with 3-4 element room and a 1-3-1-3-1 room formation
+}
+
+class player {
+
+    constructor(playerInv, playerLoc) { 
+        this.playerInv = playerInv;
+        this.playerLoc = playerLoc;
+
     }
-
-    roomsList = ["car"];
-
-    createCar() {
-        //put in background image
-    }
-
-
+}
     
 
-}
-
-function changeRooms () {
-    //location = new room
-    //change background and sprites
+function changeRooms (target) {
+    let currentLoc = player.playerLoc
+    //change background and sprites through functions in room class with target input
 }
