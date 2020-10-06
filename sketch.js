a7d16f3922f47781bbef6e9d2f6f4585d046c28b
DartFrog3 <colerobins03@gmail.com>
2020-10-06 09:53:31 -0700

    Created rooms and nav structure

--- sketch.js
+++ sketch.js
@@ -8,6 +8,17 @@ let arrowDown;
 let arrowDownImg;
 let dirKey;
 let carImg;
+let artImg;
+let hallwayImg;
+let catacombsImg;
+let stairsImg;
+let bedroomImg;
+let computerImg;
+let gymImg;
+let livingImg;
+let walkwayImg;
+let secretImg;
+let endImg;
 let rooms;
 
 
@@ -17,17 +28,27 @@ function preload() {
     arrowUpImg = loadImage('sprites/arrowUp.png');
     arrowDownImg = loadImage('sprites/arrowDown.png');
     carImg = loadImage('assets/car.jpg');
-
+    artImg = loadImage('assets/art.jpg');
+    hallwayImg = loadImage('assets/hallway.jpg');
+    catacombsImg = loadImage('assets/catacombs.jpg');
+    stairsImg = loadImage('assets/stairs.jpg');
+    bedroomImg = loadImage('assets/bedroom.jpg');
+    gymImg = loadImage('assets/gym.jpg');
+    computerImg = loadImage('assets/computer.jpg');
+    livingImg = loadImage('assets/living.jpg');
+    walkwayImg = loadImage('assets/walkway.jpg');
+    secretImg = loadImage('assets/secret.jpg');
+    endImg = loadImage('assets/end.jpg');
 
 }
 
 function setup() {
     createCanvas(800, 800);
-
+    rooms.car.display();
 }
 
 function draw() {
-    rooms.car.display();
+    
     createArrows();
 }
 
@@ -53,6 +74,9 @@ arrow = {
             arrowUp = createSprite(400, 65);
             arrowUpImg.resize(50, 50);
             arrowUp.addImage(arrowUpImg);
+            arrowUp.onMousePressed = function() {
+                rooms.hallway.display(); // must be edited
+            }
         }
     },
 
@@ -61,6 +85,9 @@ arrow = {
             arrowDown = createSprite(400, 700);
             arrowDownImg.resize(50, 50);
             arrowDown.addImage(arrowDownImg);
+            arrowDown.onMousePressed = function() {
+                rooms.car.display(); // must be edited
+            }
         }
     }
 }
@@ -76,10 +103,111 @@ function createArrows() { //adjust function to show only revlevant arrows
 rooms = {
     car : {
         index : 0,
+        description : '',
+        dir : ['up'],
         display : function() {
             background(carImg);
         }
     },
+
+    hallway : {
+        index : 1,
+        description : '',
+        dir : ['up', 'down'],
+        display : function() {
+            background(hallwayImg);
+        }
+    },
+
+    stairs : {
+        index : 2,
+        description : '',
+        dir : ['up', 'down'],
+        display : function() {
+            background(stairsImg);
+        }
+    },
+
+    living : {
+        index : 3,
+        description : '',
+        dir : ['down', 'right', 'left'],
+        display : function() {
+            background(livingImg);
+        }
+    },
+
+    walkway : {
+        index : 4,
+        description : '',
+        dir : ['right', 'left'],
+        display : function() {
+            background(walkwayImg);
+        }
+    },
+
+    art : {
+        index : 5,
+        description : '',
+        dir : ['left'],
+        display : function() {
+            background(artImg);
+        }
+    },
+
+    bedroom : {
+        index : 6,
+        description : '',
+        dir : ['up', 'right'],
+        display : function() {
+            background(bedroomImg);
+        }
+    },
+
+    catacombs : {
+        index : 7,
+        description : '',
+        dir : ['up', 'down'],
+        display : function() {
+            background(catacombsImg);
+        }
+    },
+
+    gym : {
+        index : 8,
+        description : '',
+        dir : ['down', 'left'],
+        display : function() {
+            background(gymImg);
+        }
+    },
+
+    computer : { // final puzzle room?
+        index : 9,
+        description : '',
+        dir : ['up', 'right'],
+        display : function() {
+            background(computerImg);
+        }
+    },
+
+    secret : {
+        index : 10,
+        description : '',
+        dir : ['up', 'down'],
+        display : function() {
+            background(secretImg);
+        }
+    },
+
+    end : {
+        index : 11,
+        description : '',
+        dir : [],
+        display : function() {
+            background(endImg);
+        }
+    }
 
     // end room, several rooms (10? maybe 12) with puzzles accross them- usually one main puzzle room with 3-4 element room and a 1-3-1-3-1 room formation
 }
